eaedbd984fd0ccc6258ce1a5daafc60517a620cd
BaeKKwululu <147616300+jeongduen1@users.noreply.github.com>
2025-04-21 15:15:03 +0900

    202504210314.JCY
    Updates
    SwitchTile.cs
    Switch Sprite Locked

--- Assets/Scripts/SwitchTile.cs
+++ Assets/Scripts/SwitchTile.cs
@@ -2,17 +2,9 @@ using UnityEngine;
 
 public class SwitchTile : Tile
 {
-    public SpriteRenderer myRenderer;
-
-    private void Start()
-    {
-        myRenderer = GetComponent<SpriteRenderer>();
-    }
-
     public override void OnClicked()
     {
         TileManager.Instance.ToggleLightsAround(x, y);
-        myRenderer.flipY = true;
     }
 
     private void OnMouseDown() => OnClicked();
